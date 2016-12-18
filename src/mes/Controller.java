07cef8f312f6ca297c7ab6ca63e5e0f1937716d3
marioxavier <mariomiguelxavier@gmail.com>
2016-12-18 17:09:03 +0000

    Merge origin/master
    Conflicts:
    	src/mes/Factory.java
    	src/mes/Monitor.java

--- src/mes/Controller.java
+++ src/mes/Controller.java
@@ -5,10 +5,71 @@
  */
 package mes;
 
+import java.util.ArrayList;
+import java.util.List;
+import net.wimpi.modbus.util.BitVector;
+
+class WriteModbus
+{
+    private BitVector mesOrder;
+    private int offset;
+    
+    /**
+     * Sets factory buffer
+     * @param newOrder
+     * @return 
+     */
+    public boolean setMesOrder(BitVector newOrder)
+    {
+        if (null == newOrder)
+        {
+            System.out.println("No bitvector given.\n");
+            return false;
+        }
+        else
+        {
+            mesOrder = newOrder;
+            return true;
+        }
+    }
+    
+   /**
+    * Gets MES order
+    * @return 
+    */
+    public BitVector getMesOrder()
+    {
+        return mesOrder;
+    }
+    
+    /**
+     * Sets the new offset
+     * @param newOffset
+     * @return 
+     */
+    public boolean setOffset(int newOffset)
+    {
+        offset = newOffset;
+        return true;
+    }
+    
+    /**
+     * Gets offset
+     * @return 
+     */
+    public int getOffset()
+    {
+        return offset;
+    }
+}
+
+
 /**
  *
  * @author Utilizador
  */
-public class Controller {
+public class Controller extends Thread
+{
+    List<WriteModbus> factoryBuffer = new ArrayList<>();
     
 }
